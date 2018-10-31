3fef11de5aaa3e277aff099b8f3786074c3d3efb
anwar <anwar.lynch@gmail.com>
2018-10-31 14:44:45 -0500

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(hasEqualSum(1,1,1)); //false because not equal 1
+        System.out.println(hasEqualSum(1,1,2)); //true because equal 2
+        System.out.println(hasEqualSum(1,-1,0)); //true because equal 0
+    }
+
+    public static boolean hasEqualSum(int var1, int var2, int var3) {
+        int sum = var1+var2;
+        if (sum == var3) {
+            return true;
+        }
+        return false;
     }
 }
